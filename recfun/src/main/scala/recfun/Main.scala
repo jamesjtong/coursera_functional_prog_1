16c0ab6e53d18bdec523ba9a741cf943ab3b7f95
jamesjtong <jtong@yhmg.com>
2016-09-26 13:38:44 -0400

    finish week 1 assignment recursion

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -13,15 +13,58 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
-  
+
+    def pascal(c: Int, r: Int): Int = {
+      def outOfBounds(c: Int, r: Int): Boolean = {
+        val negative  = c < 0 || r < 0
+        val pastMaxColumn = c > r
+        negative || pastMaxColumn
+      }
+      val bothTopWithinBounds = !outOfBounds(c-1, r-1) && !outOfBounds(c, r-1)
+
+      if (bothTopWithinBounds) {
+        val upperLeft = pascal(c-1, r-1)
+        val upperRight = pascal(c, r-1)
+        upperLeft + upperRight
+      } else {
+        1
+      }
+    }
+
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
-  
+    def balance(chars: List[Char]): Boolean = {
+      def isParenBalanced(chars: List[Char], numberofOpen: Int): Boolean = {
+        if (chars.isEmpty) {
+          numberofOpen == 0
+        } else if (chars.head == '(') {
+          isParenBalanced(chars.tail, numberofOpen+1)
+        } else if (chars.head == ')') {
+          numberofOpen > 0 && isParenBalanced(chars.tail, numberofOpen-1)
+        } else {
+          isParenBalanced(chars.tail, numberofOpen)
+        }
+
+      }
+      isParenBalanced(chars, 0)
+    }
+
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int = {
+      if ((money < 0) || coins.isEmpty) {
+        0
+      }
+      else {
+        if (money == 0) {
+          1
+        }
+        else {
+          countChange(money - coins.head, coins) + countChange(money, coins.tail)
+        }
+      }
+    }
+
   }
